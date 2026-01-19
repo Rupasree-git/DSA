760adf726cd5bbc7a6bbc88aa525bcf7068b8c2e
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-01-19 20:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (46.58%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,16 +4,14 @@ public:
         int start=0,end=arr.size()-1,mid;
         while(start<=end){
             mid=end+(start-end)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                return mid;
-            }
-           else if(arr[mid]>arr[mid-1]){
-            start=mid+1;
-           }
-           else{
-            end=mid-1;
-           }
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+            return mid;
+            else if(arr[mid]>arr[mid-1])
+                start=mid+1;
+            else
+                end=mid-1;
+            
         }
         return -1;
-    }
+    } 
 };
