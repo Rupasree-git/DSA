9ee543477d8f9e610d45f941dfb982a0aa38f5d3
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-01-28 00:08:46 +0530

    Time: 1 ms (96.60%) | Memory: 42.8 MB (33.30%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Use long to avoid overflow
+            long sq = (long) mid * mid;
+
+            if (sq == x) {
+                return mid;
+            } else if (sq < x) {
+                ans = mid;      // possible answer
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
