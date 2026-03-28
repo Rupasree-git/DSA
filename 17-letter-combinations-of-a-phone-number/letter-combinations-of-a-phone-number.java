cd81d5d2357fedfebea1f3aab5efb45a5a3cf07b
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-03-29 00:37:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (37.83%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,38 +1,42 @@
 import java.util.*;
 
 class Solution {
-    
     public List<String> letterCombinations(String digits) {
-        
         List<String> result = new ArrayList<>();
         
-        if(digits.length() == 0){
+        if (digits == null || digits.length() == 0) {
             return result;
         }
-
-        String[] phoneMap = {
-            "", "", "abc", "def", "ghi",
-            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        
+        String[] map = {
+            "",     // 0
+            "",     // 1
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
         };
-
-        backtrack(result, phoneMap, digits, "", 0);
-
+        
+        backtrack(result, digits, map, new StringBuilder(), 0);
         return result;
     }
-
-    public void backtrack(List<String> result, String[] phoneMap,
-                          String digits, String current, int index){
-
-        if(index == digits.length()){
-            result.add(current);
+    
+    private void backtrack(List<String> result, String digits, String[] map, StringBuilder current, int index) {
+        if (index == digits.length()) {
+            result.add(current.toString());
             return;
         }
-
-        String letters = phoneMap[digits.charAt(index) - '0'];
-
-        for(char c : letters.toCharArray()){
-            backtrack(result, phoneMap, digits,
-                      current + c, index + 1);
+        
+        String letters = map[digits.charAt(index) - '0'];
+        
+        for (char c : letters.toCharArray()) {
+            current.append(c);
+            backtrack(result, digits, map, current, index + 1);
+            current.deleteCharAt(current.length() - 1); // backtrack
         }
     }
 }
