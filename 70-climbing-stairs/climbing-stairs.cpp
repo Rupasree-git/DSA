9d2b056f29d7f37f8894cd833095c6177b468eb0
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-02 21:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
+        vector <int> dp(n+1,-1);
         return count(n,dp);
-
-        
     }
-    int count(int n,vector<int>&dp){
-            if(n<0)
-            {
-                return 0;
+    int count(int n,vector<int>&dp)
+    {
+        if(n<0){
+            return 0;
+        }
+        else if(n==0){
+            return 1;
+        }
+        else{
+            if(dp[n]!=-1){
+                return dp[n];
             }
-            else if(n==0)
-            {
-                return 1;
+            else {
+                return dp[n]=count(n-1,dp)+count(n-2,dp);
             }
-            else
-            {
-                if(dp[n]==-1)
-                    return dp[n]=count(n-1,dp)+count(n-2,dp);
-                else
-                    return dp[n];
-            }
+        }
     }
 };
