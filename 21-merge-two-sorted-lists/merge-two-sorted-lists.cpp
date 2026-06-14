f2b51d99675caf75d10918cd18d08a3f7f3b5bf7
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-14 22:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (8.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,44 +11,41 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode*temp=new ListNode(0);
-        ListNode*temp1=temp;
-        ListNode*head=temp;
-        while(list1||list2)
-        {
-            if(list1==NULL && list2)
-            {
-                temp1->next = list2;
-                return head->next;
-            }
-            else if(list2==NULL && list1)
-            {
-                temp1->next = list1;
-                return head->next;
-            }
-            else if(list1==NULL && list2==NULL)
-            {
-                return NULL;
-            }
-            else
-            {
-                if(list1->val<=list2->val)
-            {
+        ListNode *temp=new ListNode(0);
+        ListNode *temp1=temp;
+        ListNode *head=temp;
+        while(list1||list2){
+            if(list1 && list2==NULL){
                 temp=new ListNode(list1->val);
                 list1=list1->next;
                 temp1->next=temp;
                 temp1=temp1->next;
             }
-            else
-            {
+            else if(list2 && list1==NULL){
                 temp=new ListNode(list2->val);
                 list2=list2->next;
                 temp1->next=temp;
                 temp1=temp1->next;
             }
+            else if(list1==NULL && list2==NULL){
+                break;
             }
+            else if(list1->val<=list2->val){
+                temp=new ListNode(list1->val);
+                list1=list1->next;
+                temp1->next=temp;
+                temp1=temp1->next;
+            }
+            else{
+                temp=new ListNode(list2->val);
+                list2=list2->next;
+                temp1->next=temp;
+                temp1=temp1->next;
+            }
+            
+             
+
         }
         return head->next;
-
     }
 };
