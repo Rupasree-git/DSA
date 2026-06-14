d23e68c9801bdfb6d04b3416238f45accd290075
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-14 22:16:10 +0530

    Time: 1 ms (100.00%) | Memory: 48.9 MB (31.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,8 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        // Ensure nums1 is smaller array
-        if(nums1.length > nums2.length){
+        
+        if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
@@ -11,33 +10,31 @@ class Solution {
 
         int low = 0, high = m;
 
-        while(low <= high){
+        while (low <= high) {
 
             int partitionX = (low + high) / 2;
-            int partitionY = (m + n + 1)/2 - partitionX;
+            int partitionY = (m + n + 1) / 2 - partitionX;
 
-            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX-1];
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
             int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
 
-            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY-1];
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
             int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
 
-            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
 
-                if((m+n)%2==0){
-                    return (Math.max(maxLeftX,maxLeftY)+Math.min(minRightX,minRightY))/2.0;
+                if ((m + n) % 2 == 0) {
+                    return ((double)Math.max(maxLeftX, maxLeftY) +
+                            Math.min(minRightX, minRightY)) / 2;
+                } 
+                else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
                 }
-                else{
-                    return Math.max(maxLeftX,maxLeftY);
-                }
-            }
 
-            else if(maxLeftX > minRightY){
-                high = partitionX-1;
-            }
-
-            else{
-                low = partitionX+1;
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
             }
         }
 
