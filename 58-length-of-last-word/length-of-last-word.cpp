9141e4317c39c80d1b48695b965bbd35a7888c1e
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-16 12:52:31 +0530

    Time: 9 ms (0.44%) | Memory: 17.7 MB (5.07%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i=0;
-        while(!s.empty() && s.back()==' ')
-        s.pop_back();
-        s=s+" ";
-        string temp="";
-        int c=0;
-        while(s[i]!='\0'){
-            if(s[i]!=' '){
-                temp=temp+s[i];
-            }
-            else{
-                c=temp.size();
-                temp="";
-            }
-            i++;
+        int l=s.size(),a=0;string str="";
+        int i=l-1;
+        while(i>0)
+        {
+            if(s[i]==' ')
+            s.pop_back();
+            else
+            break;
+            i--;
         }
+        s=s+" ";
+        int c=0;
+        l=s.size();
+        for (int i=0;i<l;i++)
+        {
+            if(s[i]!=' ')
+                str=str+s[i];
+            else 
+            {
+                
+                c=str.size();
+                str="";
+                
+            }
+        }
+        //cout<<wrd[a];
         return c;
     }
 };
