b87b85bceb4faf3ff09d82d5fb85231eb21533bf
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-16 13:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (26.62%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i=digits.size()-1;
+        digits[i]++;
+        while(i>0 && digits[i]==10)
+        {
+            digits[i]=0;
+            digits[i-1]++;
+            i--;
+        }
+        if(digits[0]==10)
+        {
+            digits[0]=0;
+            digits.insert(digits.begin(),1);
+        }
+        return digits;        
+    }
+};
