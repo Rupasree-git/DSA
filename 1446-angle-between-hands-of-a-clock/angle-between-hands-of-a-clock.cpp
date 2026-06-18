3eb34503c6ed7e911dfda6ce8334bb401b9b9979
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-18 19:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (74.16%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double minutes1=0.0,extra_hr_rot=0.0,ans1=0.0,ans2=0.0;
+        if(hour==12){
+            hour=0*30;
+        }
+        else{
+            hour=hour*30;
+
+        }
+        minutes1=minutes*6;
+        extra_hr_rot=(minutes/60.0)*30;
+        ans1=abs(minutes1-hour-extra_hr_rot);
+        ans2=abs(360-ans1);
+    return min(ans1,ans2);
+        
+    }
+};
