4a3872ecd696421df55a9135fd3325d1dedcef1b
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-21 23:42:17 +0530

    Time: 34 ms (53.50%) | Memory: 108.8 MB (5.01%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
         vector<int>freq(100001,0);
-        for(int cost :costs)
+        int ans=0,c=0;
+        for(int i :costs)
         {
-            freq[cost]++;
+            freq[i]++;
         }
-        int ans=0,m=0;
-        for(int i=1;i<=100000;i++){
-            m=min(freq[i],coins/i);
-            ans+=m;
-            coins-=i*m;
+        for(int i=1;i<=100000;i++)
+        {
+            c=min(freq[i],coins/i);
+            ans+=c;
+            coins-=(c*i);
         }
         return ans;
+
     }
 };
