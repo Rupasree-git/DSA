9e8f4187aab89b0143d9ef4fa6b928d0be9b03de
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-22 14:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (12.70%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,13 +15,17 @@ public:
         if(p==nullptr && q==nullptr){
             return true;
         }
-        if(p==nullptr || q==nullptr){
+        else if(p==nullptr || q==nullptr){
             return false;
         }
-        else if(p->val!=q->val){
-            return false;
+        else{
+            if(p->val!=q->val){
+                return false;
+            }
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+
+
         }
-        return(isSameTree(p->left,q->left)&& isSameTree(p->right,q->right));
-        
+
     }
 };
