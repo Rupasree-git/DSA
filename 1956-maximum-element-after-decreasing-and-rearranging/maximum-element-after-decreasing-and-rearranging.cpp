da9662856dee79acf334b5904b21b8d608cf45b9
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-28 21:43:50 +0530

    Time: 4 ms (88.98%) | Memory: 55.2 MB (36.72%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,10 +3,10 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         sort(arr.begin(),arr.end());
         arr[0]=1;
-        for(int i=1;i<arr.size();i++){
+        for(int i=1;i<arr.size();i++)
+        {
             arr[i]=min(arr[i],arr[i-1]+1);
         }
-        return arr.back();
-        
+        return arr[arr.size()-1];
     }
 };
