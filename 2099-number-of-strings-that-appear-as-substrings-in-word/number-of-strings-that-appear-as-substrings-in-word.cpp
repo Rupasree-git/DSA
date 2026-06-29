0a3c93fc235ed927a142d08ee456e3357a467a59
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-06-29 14:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (82.04%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+
     int numOfStrings(vector<string>& patterns, string word) {
         int c=0;
-        for(int i=0;i<patterns.size();i++){
-            if(word.contains(patterns[i])){
-                c++;
-            }
-        }
-        return c; 
+       for(int i=0;i<patterns.size();i++)
+       {
+        if(word.contains(patterns[i]))
+            c++;
+       }
+       return c;
     }
 };
