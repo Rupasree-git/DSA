b8b420a86d18176a239d293fc8aa880af3ebcd13
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-07-27 20:41:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (60.75%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -6,8 +6,8 @@ public:
         return -1;
         sort(nums.begin(),nums.end());
         int p=(nums[l]-1)*(nums[l-1]-1);
-        int q=nums[0]-1*nums[1]-1;
+        //int q=nums[0]-1*nums[1]-1;
 
-        return max(p,q);
+        return p;
     }
 };
