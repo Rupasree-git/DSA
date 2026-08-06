b1d8e2ff63de8e9d20fc2d193be4e7d51b3e7a4f
Rupasree-git <chakrabortyrupasree23@gmail.com>
2026-08-06 21:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (97.62%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int smallestNumber(int n, int t) {
-        int k=n,r=0,p=1;
-        
-        for(int i=n;i<=100;i++){
-            k=i;
-            while(k>0){
-            r=k%10;
-            p=p*r;
-            k=k/10;
-        }
-        if(p%t==0){
+        int p=0;int a=0,r=0;
+        for(int i=n;;i++)
+        {
+            p=1;
+            a=i;
+            while(a>0)
+            {
+                r=a%10;
+                p=p*r;
+                a=a/10;
+            }
+            if(p%t==0)
             return i;
         }
-        p=1;
-
-        }
-        return -1;
     }
 };
